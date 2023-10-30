601eda179a50f6bea92eeb84fb624211b1749582
Luthor9269 <mayank_aswal@live.com>
2023-10-30 13:06:18 +0800

    go get dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Luthor9269/puppy
 
 go 1.21.3
+
+require github.com/Luthor9269/dog- v0.0.0-20231030050337-7cee1b36b624 // indirect
--- puppy.go
+++ puppy.go
@@ -1,8 +1,16 @@
 package puppy
 
+import (
+	"github.com/Luthor9269/dog-"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 func Barks() string {
 	return "Woof! Woof! WOof!"
+}
+
+func BigBark() string {
+	return dog.WHenGrownUp("HELLO WORLD")
 }
